931ed3e2ca6ff5e5ebbc6c3119505e57e3b07b3a
MitchesD <majklprofik@seznam.cz>
2021-10-26 16:34:30 +0200

    Cleanup code * replace erand with RNG

--- main.cpp
+++ main.cpp
@@ -4,6 +4,22 @@
 #include <vector>
 #include <thread>
 #include <atomic>
+#include <random>
+
+struct RNG
+{
+    RNG() : gen(rd()), distribution(std::uniform_real_distribution<double>(0.0, 1.0)) { }
+
+    double Next()
+    {
+        return distribution(gen);
+    }
+
+private:
+    std::random_device rd;
+    std::mt19937 gen;
+    std::uniform_real_distribution<double> distribution;
+};
 
 struct Vec
 {
@@ -13,10 +29,10 @@ struct Vec
     Vec operator-(const Vec &b) const { return {x - b.x,y - b.y,z - b.z}; }
     Vec operator-() const { return { -x, -y, -z}; }
     Vec operator*(double b) const { return {x * b,y * b,z * b}; }
-    Vec mult(const Vec &b) const { return {x * b.x,y * b.y,z * b.z}; }
-    Vec& norm() { return *this = *this * (1.0 / std::sqrt(x * x + y * y + z * z)); }
-    double dot(const Vec &b) const { return x * b.x + y * b.y + z * b.z; } // cross:
-    Vec operator%(Vec&b) const { return {y * b.z - z * b.y,z * b.x - x * b.z,x * b.y - y * b.x }; }
+    Vec Mult(const Vec &b) const { return {x * b.x, y * b.y, z * b.z}; }
+    Vec& Norm() { return *this = *this * (1.0 / std::sqrt(x * x + y * y + z * z)); }
+    double Dot(const Vec &b) const { return x * b.x + y * b.y + z * b.z; } // cross:
+    Vec operator%(Vec& b) const { return {y * b.z - z * b.y,z * b.x - x * b.z,x * b.y - y * b.x }; }
 };
 
 struct Ray { Vec o, d; Ray(Vec o_, Vec d_) : o(o_), d(d_) { } };
@@ -30,7 +46,8 @@ struct Camera
     Camera(Vec o_, Vec dir_, Vec up_, double angle_, int res_x_, int res_y_) : eye(o_), n(dir_), res_x(res_x_), res_y(res_y_)
     {
         Vec nn = -n;
-        u = up_ % nn; u.norm();
+        u = up_ % nn;
+        u.Norm();
         v = n % u;
         h = 2.0 * std::tan((M_PI * angle_ / 180.0) / 2.0);
         w = (res_x / res_y) * h;
@@ -46,7 +63,7 @@ struct Camera
         Vec vX = u * ps.x;
         Vec vY = v * ps.y;
         Vec vZ = n * ps.z;
-        Vec ray_dir = (vX + vY + vZ).norm();
+        Vec ray_dir = (vX + vY + vZ).Norm();
         return { eye, ray_dir };
     }
 };
@@ -65,11 +82,11 @@ struct Sphere
     double Intersect(Ray const& r) const
     {
         Vec op = p - r.o; // Solve t^2*d.d + 2*t*(o-p).d + (o-p).(o-p)-R^2 = 0
-        double t, eps = 1e-4, b = op.dot(r.d), det = b * b - op.dot(op) + rad * rad;
+        double t, eps = 1e-4, b = op.Dot(r.d), det = b * b - op.Dot(op) + rad * rad;
         if (det < 0) return 0;
         else det = std::sqrt(det);
         return (t = b - det) > eps ? t : ((t = b + det) > eps ? t : 0);
-  }
+    }
 };
 
 Sphere spheres[] = { //Scene: radius, position, emission, color, material
@@ -87,6 +104,21 @@ Sphere spheres[] = { //Scene: radius, position, emission, color, material
 inline double clamp(double x) { return x < 0 ? 0 : x > 1 ? 1 : x; }
 inline int toInt(double x) { return int(std::pow(clamp(x), 1 / 2.2) * 255 + .5); }
 
+inline Vec Reflect(Vec const& d, Vec const& n)
+{
+    return d - n * 2 * n.Dot(d);
+}
+
+Vec SampleHemisphere(RNG& rng)
+{
+    double r1 = rng.Next();
+    double r2 = rng.Next();
+
+    double phi = 2 * M_PI * r1;
+    double theta = std::sqrt(1 - r2);
+    return Vec(std::cos(phi) * theta, std::sin(phi) * theta, std::sqrt(r2));
+}
+
 inline bool Intersect(const Ray &r, double &t, int &id)
 {
     double d, inf = t = 1e20;
@@ -100,45 +132,60 @@ inline bool Intersect(const Ray &r, double &t, int &id)
     return t < inf;
 }
 
-Vec Radiance(const Ray &r, int depth, unsigned short *Xi)
+Vec Radiance(const Ray &r, int depth, RNG& rng)
 {
     double t; // distance to intersection
     int id = 0; // id of intersected object
-    if (!Intersect(r, t, id)) return {}; // if miss, return black
+    if (!Intersect(r, t, id))
+        return { 0, 0, 0 }; // if miss, return black
+
     const Sphere &obj = spheres[id]; // the hit object
-    Vec x = r.o + r.d * t, n = (x - obj.p).norm(), nl = n.dot(r.d) < 0 ? n : n * -1, f = obj.c;
+    Vec x = r.o + r.d * t;
+    Vec n = (x - obj.p).Norm();
+    Vec nl = n.Dot(r.d) < 0 ? n : n * -1;
+    Vec f = obj.c;
     // russian roulette
     double p = f.x > f.y && f.x > f.z ? f.x : f.y > f.z ? f.y : f.z;
-    if (++depth > 5) { if (erand48(Xi) < p) f = f * (1 / p); else return obj.e; }
+    if (++depth > 5)
+    {
+        if (rng.Next() < p)
+            f = f * (1 / p);
+        else
+            return obj.e;
+    }
 
     if (obj.interaction == DIFF)
     {
-        double r1 = 2 * M_PI * erand48(Xi), r2 = erand48(Xi), r2s = sqrt(r2);
-        Vec w = nl, u = ((std::fabs(w.x) > .1 ? Vec(0,1) : Vec(1)) % w).norm(), v = w % u;
-        Vec d = (u * std::cos(r1) * r2s + v * sin(r1) * r2s + w * sqrt(1 - r2)).norm();
-        return obj.e + f.mult(Radiance(Ray(x, d), depth, Xi));
+        Vec w = nl, u = ((std::fabs(w.x) > .1 ? Vec(0, 1) : Vec(1)) % w).Norm(), v = w % u;
+        Vec local_dir = SampleHemisphere(rng);
+        Vec d = (u * local_dir.x + v * local_dir.y + w * local_dir.z).Norm();
+        return obj.e + f.Mult(Radiance(Ray(x, d), depth, rng));
     } else if (obj.interaction == SPEC)
-        return obj.e + f.mult(Radiance(Ray(x, r.d - n * 2 * n.dot(r.d)), depth, Xi));
+        return obj.e + f.Mult(Radiance(Ray(x, Reflect(r.d, n)), depth, rng));
 
-    Ray reflRay(x, r.d - n * 2 * n.dot(r.d)); // Ideal dielectric REFRACTION
-    bool into = n.dot(nl) > 0;                // Ray from outside going in?
-    double nc = 1, nt = 1.5, nnt = into ? nc / nt : nt / nc, ddn = r.d.dot(nl), cos2t;
-    if ((cos2t = 1 - nnt * nnt * (1 - ddn * ddn)) < 0)// Total internal reflection
-        return obj.e + f.mult(Radiance(reflRay, depth, Xi));
+    // Fresnel equation for Refraction
+    Ray reflRay(x, Reflect(r.d, n));
+    bool into = n.Dot(nl) > 0.0f;    // Ray from outside going in?
+    double nc = 1, nt = 1.5, nnt = into ? nc / nt : nt / nc;
+    double ddn = r.d.Dot(nl), cos2t;
 
-    Vec tdir = (r.d * nnt - n * ((into ? 1 : -1) * (ddn * nnt + std::sqrt(cos2t)))).norm();
-    double a = nt - nc, b = nt + nc, R0 = a * a / (b * b), c = 1 - (into ? - ddn : tdir.dot(n));
+    // (1 - ddn * ddn) = cos^2
+    if ((cos2t = 1 - nnt * nnt * (1 - ddn * ddn)) < 0) // Total internal reflection
+        return obj.e + f.Mult(Radiance(reflRay, depth, rng));
+
+    Vec tdir = (r.d * nnt - n * ((into ? 1 : -1) * (ddn * nnt + std::sqrt(cos2t)))).Norm();
+    double a = nt - nc, b = nt + nc, R0 = a * a / (b * b), c = 1 - (into ? - ddn : tdir.Dot(n));
     double Re = R0 + (1 - R0) * c * c * c * c * c, Tr = 1 - Re, P = .25 + .5 * Re, RP = Re / P, TP = Tr / (1 - P);
-    return obj.e + f.mult(depth > 2 ? (erand48(Xi) < P ?   // Russian roulette
-                        Radiance(reflRay, depth, Xi) * RP : Radiance(Ray(x, tdir), depth, Xi) * TP) :
-                        Radiance(reflRay, depth, Xi) * Re + Radiance(Ray(x, tdir), depth, Xi) * Tr);
+    return obj.e + f.Mult(depth > 2 ? (rng.Next() < P ? // Russian roulette
+                          Radiance(reflRay, depth, rng) * RP : Radiance(Ray(x, tdir), depth, rng) * TP) :
+                          Radiance(reflRay, depth, rng) * Re + Radiance(Ray(x, tdir), depth, rng) * Tr);
 }
 
 int main(int argc, char** argv)
 {
     int w = 512, h = 512, samples = argc == 2 ? std::stoi(argv[1]) : 1; // # samples
     Vec* c = new Vec[w * h];
-    Camera cam(Vec(50,52,295.6), Vec(0,-0.042612,-1).norm(), Vec(0,1,0), 29, 512, 512);
+    Camera cam(Vec(50,52,295.6), Vec(0, -0.042612, -1).Norm(), Vec(0, 1, 0), 29, 512, 512);
     std::vector<std::thread> workers; std::atomic<int> work_done;
     unsigned worker_count = std::thread::hardware_concurrency();
     unsigned worker_modulo = h % worker_count;
@@ -151,7 +198,7 @@ int main(int argc, char** argv)
         auto max = i == worker_count - 1 ? min + worker_height + worker_final_inc : min + worker_height;
         workers.emplace_back([min, max, w, samples, cam, c, &work_done]() {
             unsigned pathCount = w * (max - min);
-            unsigned short Xi[3] = {0, 0, static_cast<unsigned short>(pathCount)};
+            RNG rng;
             for (unsigned pathId = 0; pathId < pathCount; pathId++)
             {
                 unsigned x = pathId % w;
@@ -159,11 +206,11 @@ int main(int argc, char** argv)
                 Vec r(0, 0, 0);
                 for (int s = 0; s < samples; s++)
                 {
-                    double r1 = erand48(Xi) - 0.5;
-                    double r2 = erand48(Xi) - 0.5;
+                    double r1 = rng.Next() - 0.5;
+                    double r2 = rng.Next() - 0.5;
                     Ray ray = cam.GenerateRay(x + r1, y + r2);
                     ray.o = ray.o + ray.d * 140; // hack camera origin
-                    r = r + Radiance(ray, 0, Xi) * (1.0 / samples);
+                    r = r + Radiance(ray, 0, rng) * (1.0 / samples);
                 }
 
                 auto i = static_cast<unsigned>(x + y * w);
